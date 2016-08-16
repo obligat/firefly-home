7ae50cf862e69f2cb33eb8f229b95b4b878991bc
Afaren <550047450@qq.com>
2016-08-16 20:42:07 +0800

    afaren - add mongoose refresh feature

---
+++ server/tools/fixture/raw-data.js
@@ -0,0 +1,19 @@
+export default {
+  Todo: [{
+    text: 'test1'
+  }, {
+    text: 'test3'
+  }, {
+    text: 'test4'
+  }],
+  Person: [{
+    name: '同旸',
+    image: 'images/01.jgp'
+  }, {
+    name: '张莎',
+    image: 'images/02.jgp'
+  }, {
+    name: '师悦',
+    image: 'images/03.jgp'
+  }]
+};
