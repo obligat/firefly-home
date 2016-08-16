dff20d5b8e5808d3c74a66806af560cc9ae1d320
Demo-tong <18829292377@163.com>
2016-08-17 00:15:27 +0800

    tongyang - modified the check_spec

--- spec/check-spec.js
+++ spec/check-spec.js
@@ -5,13 +5,13 @@ import {checkUsername} from '../public/src/register';
 import {checkPassword} from '../public/src/register';
 
 fdescribe('checkUsername', () => {
-    it('should return correct username', () => {
+    it('should return correct username1', () => {
         let userName = '1234_zhangsha';
         let result = checkUsername(userName);
         expect(result).toEqual({err: '', data: '1234_zhangsha'});
     });
 
-    it('should return correct username', () => {
+    it('should return correct username2', () => {
         let userName = 'shiyue_123';
         let result = checkUsername(userName);
         expect(result).toEqual({err: '', data: 'shiyue_123'});
@@ -26,7 +26,7 @@ fdescribe('checkUsername', () => {
     it('should return error type info', () => {
         let userName = '123_';
         let result = checkUsername(userName);
-        expect(result).toEqual({err: '用户名长度不能少于6位', data: ''});
+        expect(result).toEqual({err: '用户名长度不能少于6位且不大于16位', data: ''});
     });
 });
 
