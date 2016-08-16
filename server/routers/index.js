defcb72e2ea726114ec5f48ae85a5880ed78fc13
Afaren <550047450@qq.com>
2016-08-16 20:40:23 +0800

    afaren - fix naming error

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,7 +2,7 @@
  * Created by afaren on 8/16/16.
  */
 const regRouter = (app) => {
-  app.use('/user', require('./user'));
+  app.use('/users', require('./users'));
 };
 
 export default regRouter;
