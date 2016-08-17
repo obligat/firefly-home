624eb009d3d60a5bfa1d12c60b267a94278a6b72
Sundaisy <daisyzs1213@163.com>
2016-08-17 11:42:40 +0800

    show roominformation

--- public/src/components/header.js
+++ public/src/components/header.js
@@ -4,16 +4,20 @@
 import React, {Component} from 'react'
 
 export default class Header extends Component{
+   
+
     render(){
         return(
-            <div className="header" >
+            <div className="header">
                 <div className="left">
                     <span><strong>萤火虫 </strong>居住自由主义</span>
                 </div>
                 <div className="right">
-                    <span><a href="#">登录</a></span>
-                    <span>短租指南</span>
-                    <div className="issue-room"><a href="#">免费发布房间</a></div>
+                    <ul>
+                        <li><a href="#">登录</a></li>
+                        <li><a href="#">短租指南</a></li>
+                        <li><a href="#">免费发布房间</a></li>
+                    </ul>
                 </div>
             </div>
         );
