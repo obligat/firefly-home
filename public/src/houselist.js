98735487660c1ecf4532768530bfc0f7e37789e1
congcong1993 <18829291885@163.com>
2016-08-18 19:27:15 +0800

    fucongcong - add houselist file

--- public/src/houselist.js
+++ public/src/houselist.js
@@ -1,16 +1,18 @@
 import React from 'react';//eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
-import LoginComponent from './components/LoginComponent';
+import HouseResource from './components/HouseResource';
 import rootReducer from './reducers/login';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunkMiddleware from 'redux-thunk'; //eslint-disable-line no-unused-vars
-
+import {requestHouseList} from './actions/houselist';
 
 const store = createStore(
         rootReducer,
         applyMiddleware(thunkMiddleware)
 );
+
+store.dispatch(requestHouseList());
 
 render(
         <Provider store={store}>
