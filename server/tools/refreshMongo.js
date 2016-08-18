7a95de78f9c726c4343326360d376b46423756b6
congcong1993 <18829291885@163.com>
2016-08-18 18:28:37 +0800

    fucongcong - creat houseDB

--- server/tools/refreshMongo.js
+++ server/tools/refreshMongo.js
@@ -1,11 +1,12 @@
 import mongoose from 'mongoose';
 import rawData from './fixture/raw-data';
 import User from '../models/User';
+import House from '../models/House';
 import dbUrl from '../../db-config';
-// import your models from ../models
 
 const modelsMap = {
-  User
+    User,
+    House
 };
 
 let docs = Object.keys(rawData);
@@ -14,14 +15,14 @@ mongoose.connect(dbUrl);
 
 
 Object.keys(rawData)
-  .forEach(v => {
-    modelsMap[v].remove(()=> {
-      modelsMap[v].create(rawData[v], ()=> {
-        docs = docs.filter(doc => doc !== v);
-        if (docs.length === 0) {
-          process.exit(0);
-        }
-      });
+    .forEach(v => {
+        modelsMap[v].remove(()=> {
+            modelsMap[v].create(rawData[v], ()=> {
+                docs = docs.filter(doc => doc !== v);
+                if (docs.length === 0) {
+                    process.exit(0);
+                }
+            });
+        });
     });
-  });
 
