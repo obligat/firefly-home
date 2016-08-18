015eacb54ee26753d76ddc260fdbe22734ad0d6f
Afaren <550047450@qq.com>
2016-08-18 16:48:03 +0800

    afaren - clean code

--- server/routers/users.js
+++ server/routers/users.js
@@ -5,29 +5,29 @@ let router = express.Router();
 
 router.get('/', (req, res)=> {
   console.log('request came ' + JSON.stringify(req.query.username));
-  User.where({username: req.query.username}).findOne((err, user)=> {
-    if (err)
-      throw err;
-
-    const exist = user ? true : false;
-
-    console.log('send ' + JSON.stringify({exist: exist}));
-
-    res.send({exist: exist});
-  });
+  User
+    .where({username: req.query.username})
+    .findOne((err, user)=> {
+      if (err)
+        throw err;
+      res.send({
+          exist: user ? true : false
+        }
+      );
+    });
 });
 
 router.get('/validation', (req, res)=> {
-  console.log('request came ' + JSON.stringify(req.query.username) +
-    JSON.stringify(req.query.password));
+  console.log('request came '
+    + JSON.stringify(req.query.username)
+    + JSON.stringify(req.query.password));
+
 
   User.where({username: req.query.username}).findOne((err, user)=> {
     if (err)
       throw err;
-    console.log('lksdjflkjskldjflksjlkdfjlksdjflkj');
 
-    console.log('here is user: ' + JSON.stringify(user));
-
+    console.log('user found: ' + JSON.stringify(user));
 
     if (user) {
       if (user.password === req.query.password) {
