3319a1ba02f494c9956ef2ee24f659cf02e45e3f
congcong1993 <18829291885@163.com>
2016-08-19 10:45:19 +0800

    fucongcong - import the rootReducer

--- public/src/houselist.js
+++ public/src/houselist.js
@@ -1,22 +1,23 @@
 import React from 'react';//eslint-disable-line no-unused-vars
 import {render} from 'react-dom';
 import HouseResource from './components/houseComponents/HouseResource';
-import rootReducer from './reducers/login';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunkMiddleware from 'redux-thunk'; //eslint-disable-line no-unused-vars
 import {requestHouseList} from './actions/houselist';
+import rootReducer from './reducers/houseResource';
+
 
 const store = createStore(
-        rootReducer,
-        applyMiddleware(thunkMiddleware)
+    rootReducer,
+    applyMiddleware(thunkMiddleware)
 );
 
 store.dispatch(requestHouseList());
 
 render(
-        <Provider store={store}>
-          <HouseResource/>
-        </Provider>,
-        document.getElementById('houselist')
+    <Provider store={store}>
+        <HouseResource/>
+    </Provider>,
+    document.getElementById('houselist')
 );
