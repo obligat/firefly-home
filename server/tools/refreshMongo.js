7a476243798f6c72f70270f9cca4d34017b7da65
Afaren <550047450@qq.com>
2016-08-20 00:06:35 +0800

    afaren - fix refreshMongo error

--- server/tools/refreshMongo.js
+++ server/tools/refreshMongo.js
@@ -1,3 +1,32 @@
-import refresh from './mongo-util';
+import mongoose from 'mongoose';
+import rawData from './fixture/raw-data';
+import User from '../models/User';
+import House from '../models/House';
+import dbUrl from '../../db-config';
 
-refresh();
+const modelsMap = {
+  User,
+  House
+};
+
+
+let docs = Object.keys(rawData);
+
+mongoose.connect(dbUrl);
+
+Object.keys(rawData)
+  .forEach(v => {
+    modelsMap[v].remove(()=> {
+      modelsMap[v].create(rawData[v], ()=> {
+        docs = docs.filter(doc => doc !== v);
+        console.log(`The data of ${v} created`); // eslint-disable-line no-console
+        if (docs.length === 0) {
+          console.log('All data refreshed'); // eslint-disable-line no-console
+          process.exit(0);
+        }
+      });
+    });
+  });
+
+
+
