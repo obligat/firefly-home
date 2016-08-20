286f855ca4a255f6358d8302c450ec6194f47b6f
shary <1287641592@qq.com>
2016-08-20 16:37:03 +0800

    shiyue - add homepage

---
+++ public/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';//eslint-disable-line no-unused-vars
+
+class Homepage extends Component {
+  render() {
+    return (
+      <h1>home page</h1>
+    );
+  }
+}
+
+export default Homepage;
+
