6bf22733dac0cdb926f99d1b13db0c753c8a8d3f
Sundaisy <daisyzs1213@163.com>
2016-08-20 12:59:40 +0800

    zhangsha - add pagejump feature

--- public/src/reducers/register.js
+++ public/src/reducers/register.js
@@ -3,7 +3,8 @@ export const createUserResult = (state = false, action)=> {
   switch (action.type) {
   case 'ADD_USER':
     return action.message;
+  default:
+    return state;
   }
-  return state;
 };
 
