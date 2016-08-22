c26406b3e83d9da3703b501d76a75cb54a9579ff
Sundaisy <daisyzs1213@163.com>
2016-08-22 20:32:38 +0800

    zhangsha - add housedetail-spec

---
+++ spec/server/router/houseDetail-spec.js
@@ -0,0 +1,18 @@
+/**
+ * Created by zhangsha on 16-8-22.
+ */
+
+import supertest from 'supertest';
+import app from '../../../server/routers/houseDetail';
+const request = supertest(app);
+
+
+describe('post', (done) => {
+  it('should return house detail with name,price,type....', (done) => {
+
+    request
+      .get('/api/houseDetails/')
+
+
+  });
+});
