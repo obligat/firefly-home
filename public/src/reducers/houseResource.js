6607023bc0fd8d70105259d9155619d0edee9e2a
Demo-tong <18829292377@163.com>
2016-08-22 16:17:06 +0800

    tongyang - add state of select city

--- public/src/reducers/houseResource.js
+++ public/src/reducers/houseResource.js
@@ -5,10 +5,12 @@ import {combineReducers} from 'redux';
 
 const houseResource = (state = [], action)=> {
   switch (action.type) {
-  case 'RECEIVE_HOUSE_RESOURCE':
-    return action.data;
-  default:
-    return state;
+    case 'RECEIVE_HOUSE_RESOURCE':
+      return action.data;
+    case'HOUSE_OF_SELECTED_CITY':
+      return action.data;
+    default:
+      return state;
   }
 };
 
