1997d6f0374c20666ac85f53a28f0861e8b1f002
Sundaisy <daisyzs1213@163.com>
2016-08-24 21:35:16 +0800

    zhangsha - exhaust carousel

--- public/src/components/Homepage.js
+++ public/src/components/Homepage.js
@@ -5,29 +5,44 @@ import Header from './Header';
 import Footer from './Footer';
 
 class Homepage extends Component {
-    render() {
-        return (
-            <div >
-                    <Header/>
+  render() {
+    const images = [
+      {
+        original: '/images/homepageImages/homepage1.jpg',
+        thumbnail: '/images/homepageImages/homepage1.jpg',
+      },
+      {
+        original: '/images/homepageImages/homepage2.jpg',
+        thumbnail: '/images/homepageImages/homepage2.jpg',
+      },
+      {
+        original: '/images/homepageImages/homepage3.jpg',
+        thumbnail: '/images/homepageImages/homepage3.jpg',
+      }
+    ];
 
-                    <ReactGallery />
+    return (
+      <div >
+        <Header/>
 
-                <div className="container">
-                    <div className="col-xs-9">
-                        <div className="p1">住在我家里吧
-                            <span className="p2">一个温暖的港湾，一次有趣的邂逅</span>
-                        </div>
-                    </div>
-                    <div className="col-xs-3 rent">
-                        <Link to="houseResource" className="btn btn-info btn-lg active"
-                              role="button"><strong>我要租房</strong></Link>
-                    </div>
-                </div>
+        <ReactGallery images={images}/>
 
-                <Footer/>
+        <div className="container">
+          <div className="col-xs-9">
+            <div className="p1">住在我家里吧
+              <span className="p2">一个温暖的港湾，一次有趣的邂逅</span>
             </div>
-        );
-    }
+          </div>
+          <div className="col-xs-3 rent">
+            <Link to="houseResource" className="btn btn-info btn-lg active"
+                  role="button"><strong>我要租房</strong></Link>
+          </div>
+        </div>
+
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default Homepage;
