ac4761af599eab2ff181d73ab2dc28fbd76a7c8e
Demo-tong <18829292377@163.com>
2016-08-24 14:32:09 +0800

    tongyang add button of price

--- public/src/components/houseComponents/SelectZone.js
+++ public/src/components/houseComponents/SelectZone.js
@@ -4,7 +4,7 @@
 import React, {Component} from 'react';//eslint-disable-line no-unused-vars
 
 class SelectZone extends Component {
-  constructor(props, selectValue) {//eslint-disable-line no-unused-vars
+  constructor(props) {//eslint-disable-line no-unused-vars
     super(props);
   }
 
@@ -15,20 +15,38 @@ class SelectZone extends Component {
     select(cityOfSelect);
   }
 
+  handleClick(){
+    let {sort} = this.props;
+    let cityOfSelect = this.refs.citySelect.value;
+
+    sort(cityOfSelect);
+  }
+
   render() {
     return (
-      <div className="col-sm-12">
-        <form>
-          <label className="col-sm-1">城市</label>
-          <select className="col-sm-1" onChange={this.handleChange.bind(this)}
-                  ref="citySelect">
-            <option value="">all</option>
-            <option value="北京">北京</option>
-            <option value="上海">上海</option>
-            <option value="成都">成都</option>
-          </select>
-        </form>
-      </div>
+      <div>
+        <div className="row">
+          <form>
+            <label className="col-sm-1">城市</label>
+            <select className="col-sm-1" onChange={this.handleChange.bind(this)}
+                    ref="citySelect">
+              <option value="">all</option>
+              <option value="北京">北京</option>
+              <option value="上海">上海</option>
+              <option value="成都">成都</option>
+            </select>
+          </form>
+        </div>
+        <div>
+          <br/>
+          <div className="row">
+            <form>
+              <label className="col-sm-1">排序</label>
+              <button type="button" className="col-sm-1" onClick={this.handleClick.bind(this)}>Price</button>
+            </form>
+          </div>
+        </div>
+      </div >
     );
   }
 }
