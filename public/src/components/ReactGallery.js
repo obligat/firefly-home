0c72d9f6373a015f119c778a0808933c03c6ba31
Sundaisy <daisyzs1213@163.com>
2016-08-24 22:19:53 +0800

    zhangsha -  extract carousel

--- public/src/components/ReactGallery.js
+++ public/src/components/ReactGallery.js
@@ -4,35 +4,19 @@ import React from 'react';
 class ReactGallery extends React.Component {
 
 
-  render() {
-
-    const images = [
-      {
-        original: '/images/homepageImages/homepage1.jpg',
-        thumbnail: '/images/homepageImages/homepage1.jpg',
-      },
-      {
-        original: '/images/homepageImages/homepage2.jpg',
-        thumbnail: '/images/homepageImages/homepage2.jpg',
-      },
-      {
-        original: '/images/homepageImages/homepage3.jpg',
-        thumbnail: '/images/homepageImages/homepage3.jpg',
-      }
-    ];
-
-    return (
-      <div>
-        <ImageGallery
-          ref={i => this._imageGallery = i}
-          items={images}
-          slideInterval={2000}
-          showThumbnails={false}
-          autoPlay={true}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ImageGallery
+                    ref={i => this._imageGallery = i}
+                    items={this.props.images}
+                    slideInterval={2000}
+                    showThumbnails={false}
+                    autoPlay={true}
+                />
+            </div>
+        );
+    }
 
 }
 
